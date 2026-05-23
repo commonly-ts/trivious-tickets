9899658cdb38e31589ef2d36253ae9333d074d67
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-05-24 02:02:46 +1200

    Added permission and misc ticket utilities

--- src/features/tickets/ticket.functions.ts
+++ src/features/tickets/ticket.functions.ts
@@ -1,5 +1,41 @@
 import optionsBuilders from "@feature/builders/ticket.options.js";
+import TicketsClient from "@feature/client/main.client.js";
+import { TicketEntity } from "@feature/database/TicketEntity.js";
 import { TicketNameFormat, TicketNameFormatParts, TicketOption } from "@typings";
+import { OverwriteResolvable, OverwriteType, PermissionFlagsBits } from "discord.js";
+
+export const baseDefaultPermissionFlags = [
+	PermissionFlagsBits.ViewChannel,
+	PermissionFlagsBits.SendMessages,
+	PermissionFlagsBits.AttachFiles,
+	PermissionFlagsBits.EmbedLinks,
+];
+
+export function defaultChannelPermissions(
+	guildId: string,
+	userId: string,
+	hide?: boolean
+): OverwriteResolvable[] {
+	const memberOverwrites: OverwriteResolvable = {
+		type: OverwriteType.Member,
+		id: userId,
+	};
+	if (hide) memberOverwrites.deny = baseDefaultPermissionFlags;
+	else memberOverwrites.allow = baseDefaultPermissionFlags;
+
+	return [
+		memberOverwrites,
+		{
+			id: guildId,
+			deny: [PermissionFlagsBits.ViewChannel],
+		},
+	];
+}
+
+export async function getTicketByChannelId(client: TicketsClient, channelId: string) {
+	const repository = client.getRepository(TicketEntity);
+	return await repository.findOneBy({ channelId });
+}
 
 export function parseTicketOptionsButtons(ticketId: number, options: TicketOption[]) {
 	return options.map((value) => optionsBuilders[value](ticketId));
