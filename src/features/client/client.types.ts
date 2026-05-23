55c4fb5290cf62b62c9b01d7d4c612913d816b5d
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-05-24 02:01:55 +1200

    Updated TicketClientOptions

--- src/features/client/client.types.ts
+++ src/features/client/client.types.ts
@@ -1,23 +1,3 @@
-import { GuildOptions } from "@typings";
+import type { GuildOptions } from "@typings";
 
-export interface TicketClientOptions extends GuildOptions {
-	/**
-	 * Things that are automatically inserted into the Trivious client
-	 *
-	 * Properties default to true if not this is not defined
-	 */
-	binds?: {
-		/**
-		 * Whether to auto deploy ticket slash commands (e.g. /ticket close)
-		 *
-		 * @default true
-		 */
-		slashCommands: boolean;
-		/**
-		 * Whether to auto bind events (e.g. button handlers)
-		 *
-		 * @default true
-		 */
-		events: boolean;
-	};
-}
+export interface TicketClientOptions extends GuildOptions {}
