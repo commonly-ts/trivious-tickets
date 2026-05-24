a1d314d817f4cab306a2dac857befff4b74c3ba3
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-05-25 10:07:11 +1200

    Completed ticket close button

--- src/features/tickets/methods/ticket.close.ts
+++ src/features/tickets/methods/ticket.close.ts
@@ -1,7 +1,13 @@
 import TicketsClient from "@feature/client/main.client.js";
+import { TicketEntity } from "@feature/database/TicketEntity.js";
 import { type TicketComponentFunction, type TicketMethodReturn, TicketStatus } from "@typings";
-import { ButtonInteraction } from "discord.js";
-import { defaultChannelPermissions, getTicketByChannelId } from "../ticket.functions.js";
+import { ButtonInteraction, EmbedBuilder, GuildChannelEditOptions } from "discord.js";
+import { createActionRow } from "trivious";
+import {
+	defaultChannelPermissions,
+	getTicketByCustomId,
+	parseTicketOptionsButtons,
+} from "../ticket.functions.js";
 
 const method = (async (
 	client: TicketsClient,
@@ -10,21 +16,46 @@ const method = (async (
 	if (!interaction.inCachedGuild()) return [false, "Not in cached guild"];
 	const { channel, channelId, guildId } = interaction;
 	if (!channelId || !channel) return [false, "Button must be used inside a channel."];
-	const ticket = await getTicketByChannelId(client, channelId);
-	if (!ticket) return [false, "Could not find ticket from channelId"];
+	const ticket = await getTicketByCustomId(client, interaction.customId);
+	if (!ticket) return [false, "Could not find ticket from customId or database"];
 	if (ticket.status !== TicketStatus.Open)
 		return [false, "Cannot close an already closed or deleted ticket"];
 	const { metadata } = ticket;
+	const repository = client.getRepository(TicketEntity);
 
-	const closedCategoryId = metadata.categories.closedCategoryId;
-	await channel.edit({
-		parent: closedCategoryId,
+	// Edit channel name and permissions
+	const options: GuildChannelEditOptions = {
 		permissionOverwrites: defaultChannelPermissions(
 			guildId,
 			ticket.openedById,
 			!!metadata.behaviour?.onClose?.removeOpenedByUser
 		),
+	};
+	if (metadata.categories.closedCategoryId) options.parent = metadata.categories.closedCategoryId;
+	await channel.edit(options);
+
+	// Edit available ticket options
+	const headerMessage = await channel.messages.fetch(ticket.headerMessageId);
+	const parsedComponents = createActionRow(
+		...parseTicketOptionsButtons(ticket.id, metadata.options.CLOSED)
+	);
+	await headerMessage.edit({ components: [parsedComponents] });
+
+	// Send closed message
+	const closedMessage = await channel.send({
+		embeds: [
+			new EmbedBuilder()
+				.setDescription(`**Ticket closed by ${interaction.member}**`)
+				.setColor("Red"),
+		],
+		components: [parsedComponents],
 	});
+
+	// Update database
+	await repository.update(
+		{ id: ticket.id },
+		{ status: TicketStatus.Closed, closedMessageId: closedMessage.id }
+	);
 
 	return [true, "Closed ticket"];
 }) as TicketComponentFunction;
